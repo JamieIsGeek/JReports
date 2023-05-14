503c22572cc2c91dc058c1a474cc22899f928557
JamieOnSootMC <jamie@sootmc.net>
2023-05-14 21:41:41 +0100

    init

---
+++ src/main/java/uk/jamieisgeek/jreports/JReports.java
@@ -0,0 +1,16 @@
+package uk.jamieisgeek.jreports;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class JReports extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
